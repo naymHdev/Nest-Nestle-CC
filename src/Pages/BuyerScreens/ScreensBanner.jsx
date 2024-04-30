9f63b1e7bebe33e5bfe36ce519eceb0e9862c52f
MacBook Pro <naym100m@gmail.com>
2024-04-30 15:23:27 +0600

    filter tabs modified:

--- src/Pages/BuyerScreens/ScreensBanner.jsx
+++ src/Pages/BuyerScreens/ScreensBanner.jsx
@@ -2,9 +2,9 @@ import bannerImage from "../../assets/search-banner.jpeg";
 import { Tab } from "@headlessui/react";
 import BuyTabPanel from "./BuyTabPanel";
 
-// function classNames(...classes) {
-//   return classes.filter(Boolean).join(" ");
-// }
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
 
 const ScreensBanner = () => {
   return (
@@ -23,13 +23,70 @@ const ScreensBanner = () => {
             <div className="w-full">
               <Tab.Group>
                 <Tab.List className=" flex items-center gap-8 font-medium  border-b-2 border-[#F1F7FF]">
-                  <Tab>Buy</Tab>
-                  <Tab>Rent</Tab>
-                  <Tab>PG</Tab>
-                  <Tab>Plot</Tab>
-                  <Tab>Commercial</Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        " focus:outline-none",
+                        selected ? " border-b-2 border-[#055BB2]" : ""
+                      )
+                    }
+                  >
+                    Buy
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        " focus:outline-none",
+                        selected ? " border-b-2 border-[#055BB2]" : ""
+                      )
+                    }
+                  >
+                    Rent
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        " focus:outline-none",
+                        selected ? " border-b-2 border-[#055BB2]" : ""
+                      )
+                    }
+                  >
+                    PG
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        " focus:outline-none",
+                        selected ? " border-b-2 border-[#055BB2]" : ""
+                      )
+                    }
+                  >
+                    Plot
+                  </Tab>
+                  <Tab
+                    className={({ selected }) =>
+                      classNames(
+                        " focus:outline-none",
+                        selected ? " border-b-2 border-[#055BB2]" : ""
+                      )
+                    }
+                  >
+                    Commercial
+                  </Tab>
                 </Tab.List>
                 <Tab.Panels className="mt-2">
+                  <Tab.Panel>
+                    <BuyTabPanel />
+                  </Tab.Panel>
+                  <Tab.Panel>
+                    <BuyTabPanel />
+                  </Tab.Panel>
+                  <Tab.Panel>
+                    <BuyTabPanel />
+                  </Tab.Panel>
+                  <Tab.Panel>
+                    <BuyTabPanel />
+                  </Tab.Panel>
                   <Tab.Panel>
                     <BuyTabPanel />
                   </Tab.Panel>
