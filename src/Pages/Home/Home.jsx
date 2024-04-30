501ab4c779998d0393cd606d110ce46f4d91691b
MacBook Pro <naym100m@gmail.com>
2024-04-30 11:22:38 +0600

    compleate a [other services section in home pages]

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../Components/Banner/Banner";
 import FindProperty from "./FindProperty";
+import OtherServices from "./OtherServices";
 
 const Home = () => {
   return (
     <>
       <Banner />
       <FindProperty />
+      <OtherServices />
     </>
   );
 };
