e447cbd6e7b53f5555846773a95c5d09428651af
MacBook Pro <naym100m@gmail.com>
2024-05-02 00:41:57 +0600

    compleate a property card section

--- src/Pages/SearchResult/PropertyCard.jsx
+++ src/Pages/SearchResult/PropertyCard.jsx
@@ -1,20 +1,14 @@
 /* eslint-disable react/prop-types */
 import { TfiLocationPin } from "react-icons/tfi";
+import { PiSpinnerGapBold } from "react-icons/pi";
+import { GrStatusInfo } from "react-icons/gr";
+import { GiSquare } from "react-icons/gi";
 
 const PropertyCard = ({ property }) => {
-  const {
-    images,
-    price,
-    location,
-    property_name,
-    details,
-    transaction_type,
-    facing,
-    carpet_area,
-  } = property || {};
+  const { images, price, location, property_name, details } = property || {};
   return (
     <>
-      <div className=" flex gap-8 bg-[#F9FAFB] p-2 mt-5 rounded-md w-8/12 shadow-sm">
+      <div className=" flex gap-8 bg-[#F9FAFB] p-6 mt-5 rounded-md w-8/12 shadow-sm">
         <div>
           <div className="bg-[#F9FAFB]">
             {images?.map((img, index) => (
@@ -51,6 +45,39 @@ const PropertyCard = ({ property }) => {
                 <h3 className=" font-medium">Property details</h3>
               </div>
               <div className=" border-b-2 w-9/12" />
+            </div>
+            <div className=" mt-4 flex items-center justify-between">
+              <div className=" flex items-center gap-3">
+                <div className="bg-[#EE6612] rounded-md p-1">
+                  <PiSpinnerGapBold className=" text-2xl text-white" />
+                </div>
+                <div>
+                  <h3 className=" font-medium">Total Area</h3>
+                  <p className="text-sm">
+                    {details?.carpet_area} <span>sqft</span>
+                  </p>
+                </div>
+              </div>
+              <div className=" flex items-center gap-3">
+                <div className="bg-[#EE6612] rounded-md p-1">
+                  <GrStatusInfo className=" text-2xl text-white" />
+                </div>
+                <div>
+                  <h3 className=" font-medium">Status</h3>
+                  <p className="text-sm">Ready to move</p>
+                </div>
+              </div>
+              <div className=" flex items-center gap-3">
+                <div className="bg-[#EE6612] rounded-md p-1">
+                  <GiSquare className=" text-2xl text-white" />
+                </div>
+                <div>
+                  <h3 className=" font-medium">Construction Time</h3>
+                  <p className="text-sm">
+                    {details?.age_of_construction} <span>Year</span>
+                  </p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
