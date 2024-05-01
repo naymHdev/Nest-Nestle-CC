262ae1b32a793f6b58f48cab9f358284bede5d6b
MacBook Pro <naym100m@gmail.com>
2024-05-01 11:13:22 +0600

    [added] a about pages banner

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -5,6 +5,7 @@ import OurTeem from "./OurTeem";
 const About = () => {
   return (
     <>
+      <div className="bg-[url('https://i.ibb.co/CMZFm1q/about-banner.jpg')] h-64 bg-cover bg-center"></div>
       <div className=" w-11/12 mx-auto">
         <Stats />
         <OurTeem />
