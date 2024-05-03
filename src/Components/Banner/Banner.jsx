419662eae71cdd61bcc5a090bde08edfa4ea4d8d
MacBook Pro <naym100m@gmail.com>
2024-05-03 10:15:23 +0600

    updated and banner section modified and compleate a responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ import video from "../../assets/banner.mp4";
 const Banner = () => {
   return (
     <>
-      <div className="relative h-[50vh]">
+      <div className="relative h-[70vh]">
         <div className="absolute inset-0 bg-black opacity-80 z-10"></div>
         <div className="flex w-full flex-col h-full">
           <video
@@ -19,17 +19,17 @@ const Banner = () => {
         <div className="absolute top-[100px] w-full opacity-80 z-10">
           <div className="flex items-center justify-center">
             <div className="h-[50vh] flex flex-col justify-center items-center">
-              <p className="text-white text-center text-sm">
+              <p className="text-white text-center text-sm -mt-12 w-1/2 md:w-full">
                 Seamlessly connecting you to the heartbeat of India's prime
                 properties.
               </p>
-              <h2 className="text-[64px] text-white text-center">
+              <h2 className="text-3xl md:text-5xl text-white text-center mt-0 md:mt-8">
                 Your Portal to India's
               </h2>
-              <h2 className="text-[64px] -mt-8 text-white text-center">
+              <h2 className=" text-4xl md:text-5xl mt-0 md:mt-2 text-white text-center">
                 Exquisite Real Estate
               </h2>
-              <div className="flex items-center justify-center mt-5">
+              <div className="flex items-center justify-center mt-5 md:mt-8">
                 <Link to="/buyerScreens">
                   <button className="font-medium flex items-center justify-center gap-3 bg-[#055AB1] text-white rounded-md px-6 py-3">
                     <FiSearch className="text-xl" />
