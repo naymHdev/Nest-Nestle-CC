0022c68c04b34dde0ff4d207a5115ae1690d8b7b
MacBook Pro <naym100m@gmail.com>
2024-05-03 10:06:06 +0600

    fix the banner issue

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,18 +1,24 @@
-/* eslint-disable react/no-unescaped-entities */
 import { FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
-// import video from "../../assets/banner.mp4";
+import video from "../../assets/banner.mp4";
 
 const Banner = () => {
   return (
     <>
-      <div>
-        {/* <div className="">
-          <video className=" h-[50vh] w-full bg-cover opacity-55" src={video} autoPlay loop muted />
-        </div> */}
-        <div className="">
-          <div className="flex items-center justify-center bg-black">
-            <div className="h-[60vh] flex flex-col justify-center items-center">
+      <div className="relative h-[50vh]">
+        <div className="absolute inset-0 bg-black opacity-80 z-10"></div>
+        <div className="flex w-full flex-col h-full">
+          <video
+            className="w-full h-full object-cover z-0"
+            src={video}
+            autoPlay
+            loop
+            muted
+          />
+        </div>
+        <div className="absolute top-[100px] w-full opacity-80 z-10">
+          <div className="flex items-center justify-center">
+            <div className="h-[50vh] flex flex-col justify-center items-center">
               <p className="text-white text-center text-sm">
                 Seamlessly connecting you to the heartbeat of India's prime
                 properties.
